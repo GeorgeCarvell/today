83dbd6bb2b4e97721b7385ec65fec8bf68b3a8d7
GeorgeCarvell <george.carvell@pg.canterbury.ac.nz>
2017-03-13 15:58:13 +1300

    i dont know how to do git

---
+++ today-copy/src/index.js
@@ -0,0 +1,24 @@
+
+function toggle() {
+
+	var el = document.getElementById("taskform");
+
+	el.style.display = (el.style.display != 'none' ? 'none' : '' );
+
+}
+
+function hide() {
+
+	var el = document.getElementById("taskform");
+
+	el.style.display = 'none';
+
+}
+
+function show() {
+
+	var el = document.getElementById("taskform");
+
+	el.style.display = 'block';
+
+}
