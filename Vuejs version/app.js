9f04d0163fe2e339fbc8b65d97b6ac5f22b22729
George <george.carvell@pg.canterbury.ac.nz>
2017-03-29 20:25:17 +1300

    added a dummy task, task form sets name and description of dummy task, submit button adds the name of dummy task to the fakeTasks array

--- Vuejs version/app.js
+++ Vuejs version/app.js
@@ -2,6 +2,7 @@ new Vue({
     el: '#app',
     data: {
         fakeTasks: [],
+        dummyTask: {name:'', description:'', duration: 0},
         times: [
             '9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00'
         ],
@@ -12,10 +13,20 @@ new Vue({
     methods: {
         addTask: function(){
             this.fakeTasks.unshift({
-                name: 'task'
+                name: this.dummyTask.name
             });
+            this.taskFormVisible = false;
             console.log("add task");
         },
+        setName: function(event){
+        	this.dummyTask.name = event.target.value
+        },
+        setDescription: function(event){
+        	this.dummyTask.description = event.target.value
+        },
+        setDuration: function(event){
+
+        }
     }
 
 });
